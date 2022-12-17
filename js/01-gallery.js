3ebb79966c1ef2a9720fb014b74ef0821970a01f
dashevskydmytro@gmail.com <dashevskydmytro@gmail.com>
2022-12-17 23:42:53 +0200

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// -------//
+const gallery = document.querySelector('.gallery')
+
+
+
+
+function createGallery() {
+    for (const item of galleryItems) {
+        const markup = document.createElement('li');
+        markup.innerHTML = `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="Image description"
+    />
+  </a>
+</div>`;
+        gallery.append(markup);
+    }
+}
+
+gallery.addEventListener('click', openPicture);
+
+function openPicture(evt) {
+    evt.preventDefault()
+    // if (evt.target.nodeName !== 'img') {
+    //     return;
+    // }
+
+    evt.target.src = evt.target.dataset.source
+    console.log(evt.target)
+};
+
+createGallery()
